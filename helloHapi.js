0de66f057230e523ba9178d10a6e75a83f1cfcae
Jobarah <barahona.josue@unitec.edu>
2017-01-12 16:29:03 -0600

    helpers are next

--- helloHapi.js
+++ helloHapi.js
@@ -1,12 +1,14 @@
 var Hapi = require ('hapi');
 var Inert = require ('inert');
 var Vision = require ('vision');
+var Path = require('path');
+var H2o2 = require('h2o2');
 
 var server = new Hapi.Server();
 
 server.connection({
 	host: 'localhost',
-	port: Number(process.argv[2 || 8000])
+	port: Number(process.argv[2]) || 8000
 });
 
 server.register(Inert, function (err) {
@@ -17,12 +19,18 @@ server.register(Vision, function (err) {
 	if(err) throw 'Vision error';
 });
 
+server.register(H2o2, function (err) {
+    if (err) throw err;
+});
 
 server.route({
-	path: '/?name=Handling', 
+	path: '/proxy', 
 	method: 'GET',
 	handler: {
-		view:'index.html'
+		proxy: {
+			host: '127.0.0.1',
+			port: 65535
+		}
 	}
 });
 
@@ -30,8 +38,8 @@ server.views({
 	engines: {
 		html: require('handlebars')
 	},
-	paths: Path.join(__dirname, 'templates')
-})
+	path: Path.join(__dirname, 'templates')
+});
 
 server.start(function (argument) {
 	console.log('server running at: ', server.info.uri);
